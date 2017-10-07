aa24dacb574321d1a9d4d93ab7f6e7b33f55e7b8
Robert Laws <roblaws@me.com>
2017-10-07 16:14:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,1 @@
 INSERT INTO users (name, age) VALUES ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32), ('Bob', 32);
-
