fbc9dbd532e87159d66061fdb173373880fa2a2e
Robert Laws <roblaws@me.com>
2017-10-07 16:41:10 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT name, amount FROM pledges INNER JOIN users ON pledges.user_id = users.id GROUP BY name ORDER BY amount DESC"
+"SELECT name, SUM(amount) FROM pledges INNER JOIN users ON pledges.user_id = users.id GROUP BY name ORDER BY amount ASC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
